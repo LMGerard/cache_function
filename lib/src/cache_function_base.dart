d593b2c16225713f799d853a5d36b12a6b92b888
LoupioFR <lmgd@outlook.com>
2022-02-08 23:29:52 +0100

    add multiple extensions

--- lib/src/cache_function_base.dart
+++ lib/src/cache_function_base.dart
@@ -1,12 +1,41 @@
-final Map<int, Map<Comparable, dynamic>> _cache = {};
+final Map<int, Map<int, dynamic>> _cache = {};
+final Map<int, Map<int, dynamic>> _cache2 = {};
+final Map<int, Map<int, dynamic>> _cache3 = {};
+final Map<int, Map<int, dynamic>> _cacheN = {};
 
-extension CacheFunctionInt<T, K> on T Function(K) {
-  void initCache() => _cache[hashCode] = <Comparable, T>{};
-
-  Type get argType => K;
+extension CacheFunction<T, K> on T Function(K) {
+  void initCache() => _cache[hashCode] = <int, T>{};
 
   T callCached(K arg) =>
-      _cache[hashCode]!.putIfAbsent(arg as Comparable, () => this(arg)) as T;
+      _cache[hashCode]!.putIfAbsent(arg.hashCode, () => this(arg)) as T;
 
   static Map<int, Map<dynamic, dynamic>> get cache => _cache;
+}
+
+extension CacheFunction2<T, K, L> on T Function(K, L) {
+  void initCache() => _cache2[hashCode] = <int, T>{};
+
+  T callCached(K arg1, L arg2) => _cache2[hashCode]!
+      .putIfAbsent(Object.hash(arg1, arg2), () => this(arg1, arg2)) as T;
+
+  static Map<int, Map<dynamic, dynamic>> get cache => _cache2;
+}
+
+extension CacheFunction3<T, K, L, M> on T Function(K, L, M) {
+  void initCache() => _cache3[hashCode] = <int, T>{};
+
+  T callCached(K arg1, L arg2, M arg3) => _cache3[hashCode]!.putIfAbsent(
+      Object.hash(arg1, arg2, arg3), () => this(arg1, arg2, arg3)) as T;
+
+  static Map<int, Map<dynamic, dynamic>> get cache => _cache3;
+}
+
+extension CacheFunctionN<T> on T Function(Iterable) {
+  void initCache() => _cacheN[hashCode] = <int, T>{};
+
+  T callCached(args) =>
+      _cacheN[hashCode]!.putIfAbsent(Object.hashAll(args), () => this(args))
+          as T;
+
+  static Map<int, Map<dynamic, dynamic>> get cache => _cacheN;
 }
--- main.dart
+++ main.dart
@@ -3,10 +3,19 @@ import 'package:cache_function/cache_function.dart';
 void main() {
   fibo.initCache();
 
-  print(fibo.callCached(1000));
+  myFunc.initCache();
+
+  print(myFunc.callCached(10, 20));
+  print(myFunc.callCached(10, 20));
+  print(CacheFunction2.cache);
 }
 
 BigInt fibo(num n) {
   if (n < 2) return BigInt.from(n);
   return fibo.callCached(n - 1) + fibo.callCached(n - 2);
+}
+
+int myFunc(int n, int m) {
+  print('yo');
+  return n * m;
 }
